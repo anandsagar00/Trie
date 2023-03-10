ad1d9b5f076c3061bf03707f422dacc6605408c0
Anand Sagar <anandsagar152@gmail.com>
2023-03-10 16:13:26 +0530

    Added : Insert function in Trie

--- trie_class.cpp
+++ trie_class.cpp
@@ -21,6 +21,85 @@ Each node (except the root) can store one letter of the alphabet.
 
 #include<iostream>
 #include<vector>
+#include<string>
 
 using namespace std;
 
+class Trie
+{
+    vector<Trie *> children;
+    bool end_of_word;
+    public:
+    Trie()
+    {
+        //Default constructor to initialise all required variables
+        children.resize(26,NULL);
+        end_of_word=true;
+    }
+
+    //recursive function to insert string into Trie
+    void insert(string str,Trie *trie,int i=0)
+    {
+        //If it is the end of the word to insert and there is no other word with same prefix
+        if(i==str.size())
+        {
+            return;
+        }
+        else
+        {
+            //inserting the current character and calling to insert for next characters
+
+            //First I will mark this as not end of word as there is a character
+            trie->end_of_word=false;
+
+            //Initialising the children ensures that there may be more words with same prefix
+
+            //If children are null , that means this is the first time this character is being inserted on this path
+            if(!trie->children[str[i]-'a'])
+            {
+                trie->children[str[i]-'a']=new Trie();
+                insert(str,trie->children[str[i]-'a'],i+1);
+            }
+
+            //other wise it means that there is already a word with same prefix till now
+            else
+            {
+                insert(str,trie->children[str[i]-'a'],i+1);
+            }
+
+        }
+    }
+
+    void printAllWords(Trie *trie,string temp="")
+    {
+        if(trie==NULL || trie->end_of_word)
+        {
+            cout<<temp<<"\n";
+            return;
+        }
+        else
+        {
+            for(int i=0;i<26;i++)
+            {
+                if(trie->children[i])
+                {
+                    temp.push_back('a'+i);
+                    printAllWords(trie->children[i],temp);
+                    temp.pop_back();//after the call finishes back-track
+                }
+            }
+        }
+    }
+};
+
+int main()
+{
+    Trie *trie=new Trie();
+    trie->insert("hello",trie);
+    trie->insert("hell",trie);
+    trie->insert("help",trie);
+    trie->insert("apple",trie);
+    trie->insert("apps",trie);
+    trie->printAllWords(trie);
+
+}
